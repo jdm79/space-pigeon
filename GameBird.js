d239f5d62f0f7d8a8844f730077be3b546a8f92f
jdm79 <james.malvern@gmail.com>
2025-08-01 11:59:41 +0100

    enforce fullscreen on mob view in browser

--- GameBird.js
+++ GameBird.js
@@ -217,7 +217,7 @@ class SpacePigeonGame {
           this.startLevel();
         }
       }
-    } else if (this.gameState === "gameOver" && keyCode === this.keys.M) {
+    } else if (this.gameState === "gameOver" && keyCode === this.keys.SP) {
       this.returnToMainMenu();
     }
 
@@ -227,7 +227,7 @@ class SpacePigeonGame {
   handleKeyUp(keyCode) {
     if (keyCode === this.keys.SP) {
       this.player.handleInput(keyCode, false);
-    } else if (keyCode === this.keys.M && this.gameState === "gameOver") {
+    } else if (keyCode === this.keys.SP && this.gameState === "gameOver") {
       this.returnToMainMenu();
     }
 
@@ -496,10 +496,12 @@ class SpacePigeonGame {
       this.previousHighScore
     );
 
-    // Instructions - bright green
+    // Instructions - bright green (different for mobile vs desktop)
     this.canvasContext.font = "16px VT323, monospace";
     this.canvasContext.fillStyle = "#00FF00";
-    this.canvasContext.fillText("Press M to return to menu", centerX, 365);
+    const isMobile = 'ontouchstart' in window && window.innerWidth <= 768;
+    const instructionText = isMobile ? "Touch screen to return to menu" : "Press SPACE to return to menu";
+    this.canvasContext.fillText(instructionText, centerX, 365);
 
     // Reset text alignment
     this.canvasContext.textAlign = "left";
